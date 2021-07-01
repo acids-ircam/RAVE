28ee0bbdd5427f9d3ef346ab56b7f3b013383418
caillonantoine <caillon@ircam.fr>
2021-07-01 09:41:56 +0200

    add public header

--- realtime/backend/backend_public.h
+++ realtime/backend/backend_public.h
@@ -1,18 +1,11 @@
 #pragma once
-#include "backend_public.h"
 #include <string>
-#include <torch/script.h>
-#include <torch/torch.h>
 #include <vector>
 
-class Backend : public BackendPublic {
-private:
-  torch::jit::script::Module m_model;
-  int m_loaded;
-
+class BackendPublic {
 public:
-  Backend();
-  ~Backend();
+  BackendPublic();
+  ~BackendPublic();
   void perform(std::vector<float *> in_buffer, std::vector<float *> out_buffer,
                int n_vec, std::string method);
   int load(std::string path);
