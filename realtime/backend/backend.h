dbf0cc0f4dfa3010e6a17679b713594264bab5a7
caillonantoine <caillon@ircam.fr>
2021-07-01 10:30:26 +0200

    start working on max external

--- realtime/backend/backend.h
+++ realtime/backend/backend.h
@@ -1,18 +1,16 @@
 #pragma once
-#include "backend_public.h"
 #include <string>
 #include <torch/script.h>
 #include <torch/torch.h>
 #include <vector>
 
-class Backend : public BackendPublic {
+class Backend {
 private:
   torch::jit::script::Module m_model;
   int m_loaded;
 
 public:
   Backend();
-  ~Backend();
   void perform(std::vector<float *> in_buffer, std::vector<float *> out_buffer,
                int n_vec, std::string method);
   int load(std::string path);
