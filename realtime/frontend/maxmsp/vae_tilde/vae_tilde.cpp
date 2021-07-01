b771cc7763911a81041fbfb0c01adb91eebd512b
caillonantoine <caillon@ircam.fr>
2021-07-01 13:43:10 +0200

    add operator

--- realtime/frontend/maxmsp/vae_tilde/vae_tilde.cpp
+++ realtime/frontend/maxmsp/vae_tilde/vae_tilde.cpp
@@ -1,5 +1,6 @@
 #include "../../../backend/backend.h"
 #include "c74_min.h"
+#include <thread>
 #include <vector>
 
 #define BUFFER_SIZE 2048
@@ -15,7 +16,12 @@ public:
   inlet<> m_audio_input{this, "(signal) Audio input", "signal"};
   outlet<> m_audio_output{this, "(signal) Audio output", "signal"};
 
+  vae() : m_head(0), compute_thread(nullptr) {}
+
   Backend m_model;
+  float m_in_buffer[2 * BUFFER_SIZE], m_out_buffer[2 * BUFFER_SIZE];
+  int m_head;
+  std::thread *compute_thread;
 
   message<> load{this, "load", "load a pretrained ai model into the external",
                  MIN_FUNCTION{auto status = m_model.load(args[0]);
@@ -30,8 +36,42 @@ public:
 }
 ;
 
-void operator()(audio_bundle input, audio_bundle output) {}
+void operator()(audio_bundle input, audio_bundle output);
 }
 ;
+
+void thread_perform(vae *vae_instance, std::vector<float *> in_buffer,
+                    std::vector<float *> out_buffer, int n_vec,
+                    std::string method) {
+  vae_instance->m_model.perform(in_buffer, out_buffer, n_vec, method);
+}
+
+void vae::operator()(audio_bundle input, audio_bundle output) {
+
+  auto in = input.samples(0);
+  auto out = output.samples(0);
+
+  for (int i(0); i < input.frame_count(); i++) {
+    m_in_buffer[i + m_head] = float(in[i]);
+    out[i] = double(m_out_buffer[i + m_head]);
+  }
+
+  m_head += input.frame_count();
+
+  if (!(m_head % BUFFER_SIZE)) {
+    if (compute_thread) {
+      compute_thread->join();
+    }
+
+    m_head = m_head % (2 * BUFFER_SIZE);
+
+    int offset_head = (m_head + BUFFER_SIZE) % (2 * BUFFER_SIZE);
+    std::vector<float *> in_buffer{m_in_buffer + offset_head};
+    std::vector<float *> out_buffer{m_out_buffer + offset_head};
+
+    compute_thread = new std::thread(thread_perform, this, in_buffer,
+                                     out_buffer, BUFFER_SIZE, "forward");
+  }
+}
 
 MIN_EXTERNAL(vae);
