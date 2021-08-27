d811ab1b57cbed32f8f85b892a43de0c4fe4e55e
caillonantoine <caillon@ircam.fr>
2021-08-27 10:13:34 +0200

    update debug

--- debug.py
+++ debug.py
@@ -1,48 +1,46 @@
-# %%
 import torch
 
 torch.set_grad_enabled(False)
-import torch.nn as nn
 
-from fd.parallel_model.model import ParallelModel
-
-model = ParallelModel.load_from_checkpoint(
-    "lightning_logs/version_0/checkpoints/last.ckpt").eval()
-
-
-def generate(z, generator):
-    z = generator.pre_net(z)
-    z = generator.net(z)
-    z = generator.post_net(z)
-
-    loudness = z[:, :1]
-    waveform = z[:, 1:]
-
-    waveform = torch.tanh(waveform) * loudness
-
-    return waveform, loudness
-
-
-# %%
 import librosa as li
 from random import choice
 from glob import glob
-import matplotlib.pyplot as plt
-import torch
 
-x, sr = li.load(
-    choice(
-        glob("/slow-2/antoine/dataset/ljspeech/LJSpeech-1.1/out_24k/*.wav")),
-    None)
+import sounddevice as sd
+from tqdm import tqdm
 
+from fd.parallel_model.model import ParallelModel
+
+
+def play(x):
+    sd.play(x.reshape(-1).numpy(), 48000)
+    sd.wait()
+
+
+audio = choice(
+    glob("/Users/acaillon/Downloads/The Wheel of Time/00 - New Spring/*.wav"))
+x, sr = li.load(audio, 48000)
 x = x[:2**16]
+
+model_script = torch.jit.load("vae.ts").eval()
+model_check = ParallelModel.load_from_checkpoint("checkpoints/last-v1.ckpt", strict=False).eval()
+
+def generate(x):
+    if model_check.pqmf is not None:
+        x = model_check.pqmf(x)
+
+    mean, scale = model_check.encoder(x)
+    z, _ = model_check.reparametrize(mean, scale)
+    y = model_check.decoder(z)
+
+    if model_check.pqmf is not None:
+        y = model_check.pqmf.inverse(y)
+    return y
+
 x = torch.from_numpy(x).float().reshape(1, 1, -1)
+y_script = model_script(x)
+y_check = generate(x)
 
-z = model.reparametrize(*model.encoder(model.pqmf(x)))[0]
-
-l_true = model.loudness(x)
-
-y, l_fake = generate(z, model.decoder)
-
-# plt.plot(l_true.reshape(-1))
-plt.plot(l_fake.reshape(-1))
+play(x)
+play(y_check)
+play(y_script)
