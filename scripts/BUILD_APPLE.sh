fb3efa72b921bb31ac7d313bdbe7c4a8b7a65d75
caillonantoine <caillon@ircam.fr>
2021-08-27 11:12:03 +0200

    add apple biuld

---
+++ scripts/BUILD_APPLE.sh
@@ -0,0 +1,26 @@
+LIBTORCH="https://download.pytorch.org/libtorch/cpu/libtorch-macos-1.9.0.zip"
+
+echo "building for apple"
+
+# GET LIBTORCH
+if [ ! -d "libtorch/" ]
+then
+echo "downloading libtorch"
+curl $LIBTORCH -o libtorch.zip &> /dev/null
+echo "unzipping libtorch"
+unzip libtorch.zip &> /dev/null
+rm libtorch.zip
+else
+echo "libtorch found"
+fi
+
+# CONFIGURE PROJECT
+echo "configuring project"
+cd libtorch
+LIBTORCH=$(pwd)
+cd ../../realtime
+mkdir -p build
+cd build
+cmake ../ -DCMAKE_PREFIX_PATH=$LIBTORCH &> /dev/null
+echo "building project"
+make &> /dev/null
