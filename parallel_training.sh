62ebe68e27429c13e17956ee0ea32dede4205bdb
Antoine <caillon@ircam.fr>
2022-02-15 12:15:59 +0100

    refactor parallel training

--- parallel_training.sh
+++ parallel_training.sh
@@ -1,12 +1,19 @@
+function cleanup(){
+    rm job_*.sh
+    rm jobs.input
+}
+
+
+cleanup
+
 INDEX=0
 for file in $(ls instruction_*.txt)
 do
-    cat $file | grep python >> job_$INDEX.sh
+    cat $file | grep python > job_$INDEX.sh
     echo job_$INDEX.sh >> jobs.input
-   ((INDEX++))
+    ((INDEX++))
 done
 
 cat jobs.input | parallel -j$1 'CUDA_VISIBLE_DEVICES=$(({%}-1)) source {} > job_$(({#}-1)).log 2>&1'
 
-rm job_*.sh
-rm jobs.input
+cleanup
