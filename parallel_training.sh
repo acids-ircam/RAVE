11e0ee9e496192baa1dc201e9127c4844e706c01
Antoine <caillon@ircam.fr>
2022-02-15 12:03:42 +0100

    add parallel training script

---
+++ parallel_training.sh
@@ -0,0 +1,12 @@
+INDEX=0
+for file in $(ls instruction_*.txt)
+do
+    cat $file | grep python >> job_$INDEX.sh
+    echo job_$INDEX.sh >> jobs.input
+   ((INDEX++))
+done
+
+cat jobs.input | parallel -j$1 'CUDA_VISIBLE_DEVICES=$(({%}-1)) source {} > job_$(({#}-1)).log 2>&1'
+
+rm job_*.sh
+rm jobs.input
