d7f1ab7f8e8801783fc001558610f887fc79a63b
Antoine <caillonantoine@gmail.com>
2022-03-28 13:54:28 +0200

    proper rave alignment

--- export_prior.py
+++ export_prior.py
@@ -3,8 +3,6 @@ import torch
 torch.set_grad_enabled(False)
 import torch.nn as nn
 from effortless_config import Config
-from glob import glob
-from os import path
 import logging
 from termcolor import colored
 
--- rave/model.py
+++ rave/model.py
@@ -11,15 +11,10 @@ from einops import rearrange
 
 from time import time
 
-from cached_conv import USE_BUFFER_CONV, get_padding
-from cached_conv import CachedConv1d, CachedConvTranspose1d, Conv1d, AlignBranches, CachedSequential
-
-Conv1d = CachedConv1d if USE_BUFFER_CONV else Conv1d
-ConvTranspose1d = CachedConvTranspose1d if USE_BUFFER_CONV else nn.ConvTranspose1d
+import cached_conv as cc
 
 
 class Profiler:
-
     def __init__(self):
         self.ticks = [[time(), None]]
 
@@ -37,16 +32,15 @@ class Profiler:
 
 
 class Residual(nn.Module):
-
-    def __init__(self, module):
+    def __init__(self, module, cumulative_delay=0):
         super().__init__()
-        future = module.future_compensation
-        self.aligned = AlignBranches(
+        delay = module.cumulative_delay
+        self.aligned = cc.AlignBranches(
             module,
             nn.Identity(),
-            futures=[future, 0],
+            futures=[delay, 0],
         )
-        self.future_compensation = future
+        self.cumulative_delay = delay + cumulative_delay
 
     def forward(self, x):
         x_net, x_res = self.aligned(x)
@@ -54,56 +48,70 @@ class Residual(nn.Module):
 
 
 class ResidualStack(nn.Module):
-
-    def __init__(self, dim, kernel_size, padding_mode, bias=False):
+    def __init__(self,
+                 dim,
+                 kernel_size,
+                 padding_mode,
+                 cumulative_delay=0,
+                 bias=False):
         super().__init__()
         net = []
-        for i in range(3):
-            net.append(
-                Residual(
-                    CachedSequential(
-                        nn.LeakyReLU(.2),
-                        wn(
-                            Conv1d(
-                                dim,
-                                dim,
-                                kernel_size,
-                                padding=get_padding(
-                                    kernel_size,
-                                    dilation=3**i,
-                                    mode=padding_mode,
-                                ),
-                                dilation=3**i,
-                                bias=bias,
-                            )),
-                        nn.LeakyReLU(.2),
-                        wn(
-                            Conv1d(
-                                dim,
-                                dim,
-                                kernel_size,
-                                padding=get_padding(kernel_size,
-                                                    mode=padding_mode),
-                                bias=bias,
-                            )),
-                    )))
 
-        self.net = CachedSequential(*net)
-        self.future_compensation = self.net.future_compensation
+        res_cum_delay = 0
+        # SEQUENTIAL RESIDUALS
+        for i in range(3):
+            # RESIDUAL BLOCK
+            seq = [nn.LeakyReLU(.2)]
+            seq.append(
+                wn(
+                    cc.Conv1d(dim,
+                              dim,
+                              kernel_size,
+                              padding=cc.get_padding(
+                                  kernel_size,
+                                  dilation=3**i,
+                                  mode=padding_mode,
+                              ),
+                              dilation=3**i,
+                              bias=bias)))
+
+            seq.append(nn.LeakyReLU(.2))
+            seq.append(
+                wn(
+                    cc.Conv1d(dim,
+                              dim,
+                              kernel_size,
+                              padding=cc.get_padding(kernel_size,
+                                                     mode=padding_mode),
+                              bias=bias,
+                              cumulative_delay=seq[-1].cumulative_delay)))
+
+            net.append(
+                Residual(cc.CachedSequential(*seq),
+                         cumulative_delay=res_cum_delay))
+            res_cum_delay = net[-1].cumulative_delay
+
+        self.net = cc.CachedSequential(*net)
+        self.cumulative_delay = self.net.cumulative_delay + cumulative_delay
 
     def forward(self, x):
         return self.net(x)
 
 
 class UpsampleLayer(nn.Module):
-
-    def __init__(self, in_dim, out_dim, ratio, padding_mode, bias=False):
+    def __init__(self,
+                 in_dim,
+                 out_dim,
+                 ratio,
+                 padding_mode,
+                 cumulative_delay=0,
+                 bias=False):
         super().__init__()
         net = [nn.LeakyReLU(.2)]
         if ratio > 1:
             net.append(
                 wn(
-                    ConvTranspose1d(
+                    cc.ConvTranspose1d(
                         in_dim,
                         out_dim,
                         2 * ratio,
@@ -114,42 +122,45 @@ class UpsampleLayer(nn.Module):
         else:
             net.append(
                 wn(
-                    Conv1d(
+                    cc.Conv1d(
                         in_dim,
                         out_dim,
                         3,
-                        padding=get_padding(3, mode=padding_mode),
+                        padding=cc.get_padding(3, mode=padding_mode),
                         bias=bias,
                     )))
 
-        self.net = CachedSequential(*net)
-        self.future_compensation = self.net.future_compensation
+        self.net = cc.CachedSequential(*net)
+        self.cumulative_delay = self.net.cumulative_delay + cumulative_delay * ratio
 
     def forward(self, x):
         return self.net(x)
 
 
 class NoiseGenerator(nn.Module):
-
     def __init__(self, in_size, data_size, ratios, noise_bands, padding_mode):
         super().__init__()
         net = []
         channels = [in_size] * len(ratios) + [data_size * noise_bands]
+        cum_delay = 0
         for i, r in enumerate(ratios):
             net.append(
-                Conv1d(
+                cc.Conv1d(
                     channels[i],
                     channels[i + 1],
                     3,
-                    padding=get_padding(3, r, mode=padding_mode),
+                    padding=cc.get_padding(3, r, mode=padding_mode),
                     stride=r,
+                    cumulative_delay=cum_delay,
                 ))
+            cum_delay = net[-1].cumulative_delay
             if i != len(ratios) - 1:
                 net.append(nn.LeakyReLU(.2))
 
-        self.net = CachedSequential(*net)
+        self.net = cc.CachedSequential(*net)
         self.data_size = data_size
-        self.future_compensation = self.net.future_compensation
+        self.cumulative_delay = self.net.cumulative_delay * int(
+            np.prod(ratios))
 
         self.register_buffer(
             "target_size",
@@ -170,7 +181,6 @@ class NoiseGenerator(nn.Module):
 
 
 class Generator(nn.Module):
-
     def __init__(self,
                  latent_size,
                  capacity,
@@ -185,39 +195,57 @@ class Generator(nn.Module):
         super().__init__()
         net = [
             wn(
-                Conv1d(
+                cc.Conv1d(
                     latent_size,
                     2**len(ratios) * capacity,
                     7,
-                    padding=get_padding(7, mode=padding_mode),
+                    padding=cc.get_padding(7, mode=padding_mode),
                     bias=bias,
                 ))
         ]
+
         for i, r in enumerate(ratios):
             in_dim = 2**(len(ratios) - i) * capacity
             out_dim = 2**(len(ratios) - i - 1) * capacity
 
-            net.append(UpsampleLayer(in_dim, out_dim, r, padding_mode))
-            net.append(ResidualStack(out_dim, 3, padding_mode))
+            net.append(
+                UpsampleLayer(
+                    in_dim,
+                    out_dim,
+                    r,
+                    padding_mode,
+                    cumulative_delay=net[-1].cumulative_delay,
+                ))
+            net.append(
+                ResidualStack(
+                    out_dim,
+                    3,
+                    padding_mode,
+                    cumulative_delay=net[-1].cumulative_delay,
+                ))
 
-        self.net = CachedSequential(*net)
+        self.net = cc.CachedSequential(*net)
 
         wave_gen = wn(
-            Conv1d(out_dim,
-                   data_size,
-                   7,
-                   padding=get_padding(7, mode=padding_mode),
-                   bias=bias))
+            cc.Conv1d(
+                out_dim,
+                data_size,
+                7,
+                padding=cc.get_padding(7, mode=padding_mode),
+                bias=bias,
+            ))
 
         loud_gen = wn(
-            Conv1d(out_dim,
-                   1,
-                   2 * loud_stride + 1,
-                   stride=loud_stride,
-                   padding=get_padding(2 * loud_stride + 1,
+            cc.Conv1d(
+                out_dim,
+                1,
+                2 * loud_stride + 1,
+                stride=loud_stride,
+                padding=cc.get_padding(2 * loud_stride + 1,
                                        loud_stride,
                                        mode=padding_mode),
-                   bias=bias))
+                bias=bias,
+            ))
 
         branches = [wave_gen, loud_gen]
 
@@ -231,9 +259,14 @@ class Generator(nn.Module):
             )
             branches.append(noise_gen)
 
-        self.synth = AlignBranches(*branches)
+        self.synth = cc.AlignBranches(
+            *branches,
+            cumulative_delay=self.net.cumulative_delay,
+        )
+
         self.use_noise = use_noise
         self.loud_stride = loud_stride
+        self.cumulative_delay = self.synth.cumulative_delay
 
     def forward(self, x, add_noise: bool = True):
         x = self.net(x)
@@ -256,7 +289,6 @@ class Generator(nn.Module):
 
 
 class Encoder(nn.Module):
-
     def __init__(self,
                  data_size,
                  capacity,
@@ -266,11 +298,11 @@ class Encoder(nn.Module):
                  bias=False):
         super().__init__()
         net = [
-            Conv1d(data_size,
-                   capacity,
-                   7,
-                   padding=get_padding(7, mode=padding_mode),
-                   bias=bias)
+            cc.Conv1d(data_size,
+                      capacity,
+                      7,
+                      padding=cc.get_padding(7, mode=padding_mode),
+                      bias=bias)
         ]
 
         for i, r in enumerate(ratios):
@@ -280,28 +312,30 @@ class Encoder(nn.Module):
             net.append(nn.BatchNorm1d(in_dim))
             net.append(nn.LeakyReLU(.2))
             net.append(
-                Conv1d(
+                cc.Conv1d(
                     in_dim,
                     out_dim,
                     2 * r + 1,
-                    padding=get_padding(2 * r + 1, r, mode=padding_mode),
+                    padding=cc.get_padding(2 * r + 1, r, mode=padding_mode),
                     stride=r,
                     bias=bias,
+                    cumulative_delay=net[-3].cumulative_delay,
                 ))
 
         net.append(nn.LeakyReLU(.2))
         net.append(
-            Conv1d(
+            cc.Conv1d(
                 out_dim,
                 2 * latent_size,
                 5,
-                padding=get_padding(5, mode=padding_mode),
+                padding=cc.get_padding(5, mode=padding_mode),
                 groups=2,
                 bias=bias,
+                cumulative_delay=net[-2].cumulative_delay,
             ))
 
-        self.net = CachedSequential(*net)
-        self.future_compensation = self.net.future_compensation
+        self.net = cc.CachedSequential(*net)
+        self.cumulative_delay = self.net.cumulative_delay
 
     def forward(self, x):
         z = self.net(x)
@@ -309,49 +343,50 @@ class Encoder(nn.Module):
 
 
 class Discriminator(nn.Module):
-
     def __init__(self, in_size, capacity, multiplier, n_layers):
         super().__init__()
 
-        net = [wn(Conv1d(in_size, capacity, 15, padding=get_padding(15)))]
+        net = [
+            wn(cc.Conv1d(in_size, capacity, 15, padding=cc.get_padding(15)))
+        ]
         net.append(nn.LeakyReLU(.2))
 
         for i in range(n_layers):
             net.append(
                 wn(
-                    Conv1d(
+                    cc.Conv1d(
                         capacity * multiplier**i,
                         min(1024, capacity * multiplier**(i + 1)),
                         41,
                         stride=multiplier,
-                        padding=get_padding(41, multiplier),
+                        padding=cc.get_padding(41, multiplier),
                         groups=multiplier**(i + 1),
                     )))
             net.append(nn.LeakyReLU(.2))
 
         net.append(
             wn(
-                Conv1d(
+                cc.Conv1d(
                     min(1024, capacity * multiplier**(i + 1)),
                     min(1024, capacity * multiplier**(i + 1)),
                     5,
-                    padding=get_padding(5),
+                    padding=cc.get_padding(5),
                 )))
         net.append(nn.LeakyReLU(.2))
-        net.append(wn(Conv1d(min(1024, capacity * multiplier**(i + 1)), 1, 1)))
+        net.append(
+            wn(cc.Conv1d(min(1024, capacity * multiplier**(i + 1)), 1, 1)))
         self.net = nn.ModuleList(net)
 
     def forward(self, x):
         feature = []
         for layer in self.net:
             x = layer(x)
-            if isinstance(layer, Conv1d):
+            if isinstance(layer, cc.Conv1d):
                 feature.append(x)
         return feature
 
 
 class StackDiscriminators(nn.Module):
-
     def __init__(self, n_dis, *args, **kwargs):
         super().__init__()
         self.discriminators = nn.ModuleList(
@@ -366,7 +401,6 @@ class StackDiscriminators(nn.Module):
 
 
 class RAVE(pl.LightningModule):
-
     def __init__(self,
                  data_size,
                  capacity,
