53aa7539b039b3503de6eaf97d632eed7ae78b7b
Antoine <caillon@ircam.fr>
2022-05-31 16:40:24 +0200

    refactor configuration

--- rave/__init__.py
+++ rave/__init__.py
@@ -1,5 +1,9 @@
 import gin
 import cached_conv as cc
+import torch
+
+gin.register(torch.nn.Conv1d, module="torch.nn")
+gin.register(torch.nn.Conv2d, module="torch.nn")
 
 cc.get_padding = gin.external_configurable(cc.get_padding, module="cc")
 cc.Conv1d = gin.external_configurable(cc.Conv1d, module="cc")
