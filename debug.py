080f5fe0cbe095de8b2f07fe5665e386d12b2549
Antoine <caillonantoine@gmail.com>
2022-07-03 13:37:56 +0200

    upgrade torch

--- debug.py
+++ debug.py
@@ -1,18 +1,3 @@
-# %%
-import gin
-import rave
-from glob import glob
+import librosa
 
-configs = glob("configs/*.gin")
-
-for config in configs:
-    if "prior" in config or "general" in config:
-        continue
-    print(f"testing {config}")
-    gin.parse_config_file(config)
-    model = rave.RAVE()
-    nel = 0
-    for p in model.parameters():
-        if p.requires_grad:
-            nel += p.numel()
-    print(f"{nel/1e6:.2f}M parameters")
+librosa.istft
