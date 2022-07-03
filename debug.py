7efbfba5954c940252f171fb009afc6424816425
Antoine <caillonantoine@gmail.com>
2022-07-03 14:30:55 +0200

    update qrave latent size

--- debug.py
+++ debug.py
@@ -1,8 +1,4 @@
-import prior
-from prior import core
-import gin
+# %%
+import torch
 
-gin.parse_config_file("configs/prior.gin")
-model = prior.Prior()
-
-print(core.get_prior_receptive_field(model))
+embed = torch.nn.Embedding(1024, 512)
--- rave/scripted_vq.py
+++ rave/scripted_vq.py
@@ -12,13 +12,21 @@ def closest_code(x, embed):
     return torch.argmin(distance, -1).reshape(batch_size, -1)
 
 
-def residual_quantize(x, embed_list):
-    y = 0.
-    index = []
-    for embed in embed_list:
-        embed_ind = closest_code(x, embed)
-        q = F.embedding(embed_ind, embed).permute(0, 2, 1)
-        x = x - q
-        y = y + q
-        index.append(embed_ind)
-    return torch.stack(index, 1)
+class SimpleQuantizer(nn.Module):
+    def __init__(self, embed_list) -> None:
+        super().__init__()
+        self.register_buffer("embed", torch.stack(embed_list, 0))
+
+    def residual_quantize(self, x):
+        y = 0.
+        index = []
+        for embed in self.embed:
+            embed_ind = closest_code(x, embed)
+            q = F.embedding(embed_ind, embed).permute(0, 2, 1)
+            x = x - q
+            y = y + q
+            index.append(embed_ind)
+        return torch.stack(index, 1)
+    
+    def retrieve_codes(self, index):
+        pass
