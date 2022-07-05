21966b7a1669f0ec785faf702154650ffea68277
Antoine <caillonantoine@gmail.com>
2022-07-05 16:29:33 +0200

    allow rave export

--- export_rave.py
+++ export_rave.py
@@ -35,13 +35,12 @@ class ScriptedRAVE(torch.nn.Module):
         self.register_buffer("fidelity", pretrained.fidelity)
 
         if isinstance(pretrained.encoder, rave.blocks.VariationalEncoder):
-            self.mode = "variational"
-            latent_size = np.argmax(
-                pretrained.fidelity.numpy() > args.FIDELITY)
+            latent_size = max(
+                np.argmax(pretrained.fidelity.numpy() > args.FIDELITY), 1)
             latent_size = 2**math.ceil(math.log2(latent_size))
             self.latent_size = latent_size
+
         elif isinstance(pretrained.encoder, rave.blocks.DiscreteEncoder):
-            self.mode = "discrete"
             self.latent_size = pretrained.encoder.num_quantizers
             self.quantizer = rave.scripted_vq.SimpleQuantizer([
                 *map(
@@ -50,7 +49,6 @@ class ScriptedRAVE(torch.nn.Module):
                 )
             ])
             del self.encoder.rvq
-
 
         x = torch.zeros(1, 1, 2**14)
         x = self.pqmf(x)
@@ -67,41 +65,22 @@ class ScriptedRAVE(torch.nn.Module):
         self.register_buffer("forward_params",
                              torch.tensor([1, 1, 2 if stereo else 1, 1]))
 
+    def post_process_latent(self, z):
+        raise NotImplementedError
+
+    def pre_process_latent(self, z):
+        raise NotImplementedError
+
     @torch.jit.export
     def encode(self, x):
         x = self.pqmf(x)
         z = self.encoder(x)
-        if self.mode == "variational":
-            z = self.encoder.reparametrize(z)[0]
-            z = z - self.latent_mean.unsqueeze(-1)
-            z = F.conv1d(z, self.latent_pca.unsqueeze(-1))
-            z = z[:, :self.latent_size]
-        elif self.mode == "discrete":
-            z = self.quantizer.residual_quantize(z)
-        else:
-            raise ValueError(f"mode {self.mode} not understood")
-
+        z = self.post_process_latent(z)
         return z
 
     @torch.jit.export
     def decode(self, z):
-        if self.stereo: z = torch.cat([z, z], 0)
-
-        if self.mode == "variational":
-            noise = torch.randn(
-                z.shape[0],
-                self.full_latent_size - self.latent_size,
-                z.shape[-1],
-            )
-            z = torch.cat([z, noise], 1)
-            z = F.conv1d(z, self.latent_pca.T.unsqueeze(-1))
-            z = z + self.latent_mean.unsqueeze(-1)
-        elif self.mode == "discrete":
-            z = self.quantizer.residual_dequantize(z)
-            z = self.encoder.add_noise_to_vector(z)
-        else:
-            raise ValueError(f"mode {self.mode} not understood")
-
+        z = self.pre_process_latent(z)
         y = self.decoder(z)
         y = self.pqmf.inverse(y)
 
@@ -112,6 +91,39 @@ class ScriptedRAVE(torch.nn.Module):
 
     def forward(self, x):
         return self.decode(self.encode(x))
+
+
+class VariationalScriptedRAVE(ScriptedRAVE):
+
+    def post_process_latent(self, z):
+        z = self.encoder.reparametrize(z)[0]
+        z = z - self.latent_mean.unsqueeze(-1)
+        z = F.conv1d(z, self.latent_pca.unsqueeze(-1))
+        z = z[:, :self.latent_size]
+        return z
+
+    def pre_process_latent(self, z):
+        noise = torch.randn(
+            z.shape[0],
+            self.full_latent_size - self.latent_size,
+            z.shape[-1],
+        )
+        z = torch.cat([z, noise], 1)
+        z = F.conv1d(z, self.latent_pca.T.unsqueeze(-1))
+        z = z + self.latent_mean.unsqueeze(-1)
+        return z
+
+
+class DiscreteScriptedRAVE(ScriptedRAVE):
+
+    def post_process_latent(self, z):
+        z = self.quantizer.residual_quantize(z)
+        return z
+
+    def pre_process_latent(self, z):
+        z = self.quantizer.residual_dequantize(z)
+        z = self.encoder.add_noise_to_vector(z)
+        return z
 
 
 class args(Config):
@@ -125,13 +137,27 @@ args.parse_args()
 cc.use_cached_conv(args.STREAMING)
 
 assert args.NAME is not None, "YOU HAD ONE JOB: GIVE A NAME !!"
+
 root = os.path.join("runs", args.NAME, "rave")
 gin.parse_config_file(os.path.join(root, "config.gin"))
 checkpoint = rave.core.search_for_run(root)
 
 pretrained = rave.RAVE()
-pretrained.load_state_dict(torch.load(checkpoint)["state_dict"])
+if checkpoint is not None:
+    pretrained.load_state_dict(torch.load(checkpoint)["state_dict"])
+else:
+    print("No checkpoint found, RAVE will remain randomly initialized")
 pretrained.eval()
+
+if isinstance(pretrained.encoder, rave.blocks.VariationalEncoder):
+    mode = "variational"
+    script_class = VariationalScriptedRAVE
+elif isinstance(pretrained.encoder, rave.blocks.DiscreteEncoder):
+    mode = "discrete"
+    script_class = DiscreteScriptedRAVE
+else:
+    raise ValueError(f"Encoder type {type(pretrained.encoder)} "
+                     "not supported for export.")
 
 x = torch.zeros(1, 1, 2**14)
 pretrained(x)
@@ -140,7 +166,7 @@ for m in pretrained.modules():
     if hasattr(m, "weight_g"):
         nn.utils.remove_weight_norm(m)
 
-scripted_rave = ScriptedRAVE(pretrained=pretrained, stereo=args.STEREO)
+scripted_rave = script_class(pretrained=pretrained, stereo=args.STEREO)
 scripted_rave = torch.jit.script(scripted_rave)
 
 torch.jit.save(scripted_rave, f"{args.NAME}.ts")
