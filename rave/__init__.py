e2ce35662bb3887798c46f7496a10caf36e23124
domkirke <axelchemla@yahoo.fr>
2023-03-28 00:10:29 +0200

    modified __init__ for gin compatibility

--- rave/__init__.py
+++ rave/__init__.py
@@ -12,9 +12,19 @@ gin.add_config_file_search_path(
         'configs',
     ))
 
-cc.get_padding = gin.external_configurable(cc.get_padding, module="cc")
-cc.Conv1d = gin.external_configurable(cc.Conv1d, module="cc")
-cc.ConvTranspose1d = gin.external_configurable(cc.ConvTranspose1d, module="cc")
+def __safe_configurable(name):
+    try: 
+        setattr(cc, name, gin.get_configurable(f"cc.{name}"))
+    except ValueError:
+        setattr(cc, name, gin.external_configurable(getattr(cc, name), module="cc"))
+
+# cc.get_padding = gin.external_configurable(cc.get_padding, module="cc")
+# cc.Conv1d = gin.external_configurable(cc.Conv1d, module="cc")
+# cc.ConvTranspose1d = gin.external_configurable(cc.ConvTranspose1d, module="cc")
+
+__safe_configurable("get_padding")
+__safe_configurable("Conv1d")
+__safe_configurable("ConvTranspose1d")
 
 from .blocks import *
 from .discriminator import *
